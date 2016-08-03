572793ec8a2a7075a18bc0219d8360984eddb221
BerthaBrenes <bertacaro1996@hotmail.com>
2016-08-02 18:59:09 -0600

    bailarina con musica
    Este ejercicio es para el ejercicio de bailarina con musica y la cancion de la pantera rosa

---
+++ bailarina.ino
@@ -0,0 +1,20 @@
+int motor_de = 10;//direccion derecha del motor
+int motor_iz =11;//direccion izquierda del motor
+void setup() {
+  // put your setup code here, to run once:
+pinMode(motor_de,OUTPUT);//define los motores como salida
+pinMode(motor_iz, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(motor_de, HIGH);
+  delay(1500);
+  digitalWrite(motor_de,LOW);
+  delay(1500);
+   digitalWrite(motor_iz, HIGH);
+  delay(1500);
+  digitalWrite(motor_iz,LOW);
+  delay(1500);
+
+}
---
+++ bailarina_con_musica.ino
@@ -0,0 +1,58 @@
+int pinzumbador = 13;    // pin del zumbador
+int  la = 440;//frencuencia de la
+int la# = 466.2; //frecuencia de la#
+int sol = 392;// frecuencia de sol
+int re# = 293.7; // frecuencia de re#
+int mi = 329.6;// frecuencia de mi
+int fa# = 370;// frecuencia de fa#
+int Do = 261.6; //frecuencia de Do'
+int si = 493.2;//frecuencia de si
+int re = 587;// frecuencia de re
+
+void setup()
+{
+}
+
+void loop()
+{
+    tone(pinzumbador,re#); // inicia la secuencia de musica de la pantera rosa
+    delay(500);
+    tone(pinzumbador,mi);  
+    delay(500);
+    tone(pinzumbador,fa#); 
+    delay(500);
+    tone(pinzumbador,sol); 
+    delay(1000);
+    tone(pinzumbador,re#); 
+    delay(500);
+    tone(pinzumbador,mi);
+    delay(500);
+    tone(pinzumbador,fa#); 
+    delay(500);
+    tone(pinzumbador,sol); 
+    delay(1000);
+    tone(pinzumbador,Do);
+    delay(500);
+    tone(pinzumbador,si);
+    delay(500);
+    tone(pinzumbador,mi); 
+    delay(500);
+    tone(pinzumbador,sol);
+    delay(500); 
+    tone(pinzumbador,si); 
+    delay(500);
+    tone(pinzumbador,la#);
+    delay(1000);
+    tone(pinzumbador,la); 
+    delay(500);
+    tone(pinzumbador,sol); 
+    delay(500);
+    tone(pinzumbador,mi); 
+    delay(500);
+    tone(pinzumbador,re); 
+    delay(500);
+    tone(pinzumbador,mi);       
+    delay(1000);            
+    noTone(pinzumbador);               // lo detiene a los dos segundos
+    delay(1000);
+}
